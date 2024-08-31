61320f6e2900a7e4edd62d8126c5f1dd5857b65e
siansxint <joshua1504.jr@gmail.com>
2024-08-31 17:22:37 -0400

    feat(logger): make logger log from cached thread-pool, so we could also listen for console commands

--- claimer/src/main/java/me/siansxint/sniper/claimer/ClaimerMain.java
+++ claimer/src/main/java/me/siansxint/sniper/claimer/ClaimerMain.java
@@ -1,9 +1,63 @@
 package me.siansxint.sniper.claimer;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.UncheckedIOException;
+import java.io.Writer;
+import java.time.Duration;
+import java.time.Instant;
+import java.time.ZoneId;
+import java.time.temporal.ChronoUnit;
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
+
 // testing purposes just by now
 public class ClaimerMain {
 
-    public static void main(String[] args) {
+    private static final char[] CHARS = "abcdefghijklmnopqrstuvwxyz_0123456789".toCharArray();
 
+    public static void main1(String[] args) {
+        String[] combinations = new String[50653];
+
+        int index = 0;
+        for (char i : CHARS) {
+            for (char j : CHARS) {
+                for (char k : CHARS) {
+                    System.out.println(index);
+                    combinations[index++] = "" + i + j + k;
+                }
+            }
+        }
+
+        try (Writer writer = new BufferedWriter(new FileWriter("names.txt"))) {
+            for (int i = 0; i < combinations.length; i++) {
+                if (i == combinations.length - 1) {
+                    writer.write(combinations[i]);
+                } else {
+                    writer.write(combinations[i] + "\n");
+                }
+            }
+        } catch (IOException e) {
+            throw new UncheckedIOException(e);
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        long start = System.nanoTime();
+        Instant from = Instant.ofEpochSecond((long) 1724974437.8757012);
+        Instant to = Instant.ofEpochSecond((long) 1724980298.33745);
+
+        ZoneId zoneId = ZoneId.of("GMT-4");
+
+        System.out.println(from);
+        System.out.println(to);
+
+        Thread.sleep(10000);
+
+        System.out.println(from.plus(37, ChronoUnit.DAYS).atZone(zoneId).toLocalDateTime());
+        System.out.println(to.plus(37, ChronoUnit.DAYS).atZone(zoneId).toLocalDateTime());
+
+        System.out.println(TimeUnit.NANOSECONDS.toSeconds(System.nanoTime() - start));
     }
 }
--- common/src/main/java/me/siansxint/sniper/common/logger/LoggerModule.java
+++ common/src/main/java/me/siansxint/sniper/common/logger/LoggerModule.java
@@ -1,13 +1,18 @@
 package me.siansxint.sniper.common.logger;
 
-import team.unnamed.inject.AbstractModule;
+import team.unnamed.inject.*;
 import team.unnamed.inject.Module;
-import team.unnamed.inject.Provides;
-import team.unnamed.inject.Singleton;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.logging.*;
+import java.io.UncheckedIOException;
+import java.util.concurrent.ExecutorService;
+import java.util.logging.ConsoleHandler;
+import java.util.logging.FileHandler;
+import java.util.logging.Formatter;
+import java.util.logging.Handler;
+import java.util.logging.LogRecord;
+import java.util.logging.Logger;
 
 public class LoggerModule extends AbstractModule implements Module {
 
@@ -15,7 +20,7 @@ public class LoggerModule extends AbstractModule implements Module {
 
     @Provides
     @Singleton
-    public Logger logger() {
+    public Logger logger(@Named("cached") ExecutorService service) {
         Logger logger = Logger.getLogger(SNIPER_NAME);
 
         File logs = new File("logs");
@@ -27,7 +32,7 @@ public class LoggerModule extends AbstractModule implements Module {
         try {
             fileHandler = new FileHandler("logs/latest.log", true);
         } catch (IOException e) {
-            throw new RuntimeException(e);
+            throw new UncheckedIOException(e);
         }
 
         Formatter formatter = new LoggerFormatter();
@@ -40,7 +45,22 @@ public class LoggerModule extends AbstractModule implements Module {
         logger.setUseParentHandlers(false);
 
         logger.addHandler(fileHandler);
-        logger.addHandler(consoleHandler);
+        logger.addHandler(new Handler() {
+            @Override
+            public void publish(LogRecord record) {
+                service.submit(() -> consoleHandler.publish(record));
+            }
+
+            @Override
+            public void flush() {
+                consoleHandler.flush();
+            }
+
+            @Override
+            public void close() throws SecurityException {
+                consoleHandler.close();
+            }
+        });
 
         return logger;
     }
