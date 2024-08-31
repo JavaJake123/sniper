4cb9f0b49130315e1300d4a87f4244a687110cdb
siansxint <joshua1504.jr@gmail.com>
2024-08-31 17:15:45 -0400

    feat(http-selector): optimize selector next method calls

--- common/src/main/java/me/siansxint/sniper/common/http/HttpClientSelector.java
+++ common/src/main/java/me/siansxint/sniper/common/http/HttpClientSelector.java
@@ -3,22 +3,39 @@ package me.siansxint.sniper.common.http;
 import org.apache.hc.client5.http.classic.HttpClient;
 import org.apache.hc.client5.http.impl.classic.HttpClients;
 
+import java.util.Deque;
 import java.util.List;
-import java.util.Random;
+import java.util.concurrent.ConcurrentLinkedDeque;
 
 public class HttpClientSelector {
 
-    private static final Random RANDOM = new Random();
     private static final HttpClient DEFAULT_HTTP_CLIENT = HttpClients.createDefault();
 
-    private final List<HttpClient> clients;
+    private final Deque<HttpClient> clients = new ConcurrentLinkedDeque<>();
 
     public HttpClientSelector(List<HttpClient> clients) {
-        this.clients = clients;
+        this.clients.addAll(clients);
     }
 
     public HttpClient next() {
-        HttpClient client = clients.size() == 1 ? clients.getFirst() : clients.get(RANDOM.nextInt(clients.size()));
-        return client == null ? DEFAULT_HTTP_CLIENT : client;
+        if (clients.isEmpty()) {
+            return DEFAULT_HTTP_CLIENT;
+        } else if (clients.size() == 1) {
+            return clients.peek();
+        } else {
+            HttpClient client = clients.poll();
+
+            if (client == null) {
+                return DEFAULT_HTTP_CLIENT;
+            } else {
+                clients.add(client);
+            }
+
+            return client;
+        }
+    }
+
+    public int size() {
+        return clients.size();
     }
 }
