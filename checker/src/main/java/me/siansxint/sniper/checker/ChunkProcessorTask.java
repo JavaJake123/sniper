29ec72023ed629ebe671cdf3cd97cd2e3558195b
siansxint <joshua1504.jr@gmail.com>
2024-08-31 17:18:09 -0400

    chore(chunk-processor): separate tasks from actual name-checker-service

---
+++ checker/src/main/java/me/siansxint/sniper/checker/ChunkProcessorTask.java
@@ -0,0 +1,181 @@
+package me.siansxint.sniper.checker;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import me.siansxint.sniper.checker.config.Configuration;
+import me.siansxint.sniper.checker.model.LastCheck;
+import me.siansxint.sniper.checker.model.NameDropTime;
+import me.siansxint.sniper.checker.model.UsernamesBulkResponse;
+import me.siansxint.sniper.common.ConsoleColors;
+import me.siansxint.sniper.common.HttpResponse;
+import me.siansxint.sniper.common.registry.TRegistry;
+import org.apache.hc.client5.http.classic.HttpClient;
+import org.apache.hc.client5.http.classic.methods.HttpPost;
+import org.apache.hc.core5.http.ContentType;
+import org.apache.hc.core5.http.Header;
+import org.apache.hc.core5.http.HttpEntity;
+import org.apache.hc.core5.http.HttpStatus;
+import org.apache.hc.core5.http.io.entity.EntityUtils;
+import org.apache.hc.core5.http.io.entity.StringEntity;
+import org.apache.hc.core5.http.message.BasicHeader;
+
+import java.io.IOException;
+import java.net.URI;
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+import java.util.Collection;
+import java.util.List;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.stream.Collectors;
+
+@SuppressWarnings("BusyWait")
+public class ChunkProcessorTask implements Runnable {
+
+    private static final URI USERNAME_BULK_FIND_URI = URI.create("https://api.mojang.com/profiles/minecraft");
+    private static final Header[] DEFAULT_HEADERS = new Header[]{
+            new BasicHeader("Accept", ContentType.APPLICATION_JSON.toString()),
+            new BasicHeader("Content-Type", ContentType.APPLICATION_JSON.toString()),
+            new BasicHeader("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:129.0) Gecko/20100101 Firefox/129.0"),
+    };
+
+    public static final AtomicInteger SUCCESSFUL_REQUESTS = new AtomicInteger(0);
+
+    private final Collection<String> chunk;
+    private final CountDownLatch latch;
+
+    private final HttpClient client;
+    private final ObjectMapper mapper;
+
+    private final Logger logger;
+
+    private final TRegistry<NameDropTime> dropTimes;
+    private final LastCheckCachedStorage lastChecks;
+
+    private final Configuration configuration;
+
+    private final Instant startedAt;
+
+    public ChunkProcessorTask(
+            Collection<String> chunk,
+            CountDownLatch latch,
+            HttpClient client,
+            ObjectMapper mapper,
+            Logger logger,
+            TRegistry<NameDropTime> dropTimes,
+            LastCheckCachedStorage lastChecks,
+            Configuration configuration,
+            Instant startedAt
+    ) {
+        this.chunk = chunk;
+        this.latch = latch;
+        this.client = client;
+        this.mapper = mapper;
+        this.logger = logger;
+        this.dropTimes = dropTimes;
+        this.lastChecks = lastChecks;
+        this.configuration = configuration;
+        this.startedAt = startedAt;
+    }
+
+    @Override
+    public void run() {
+        HttpPost request = new HttpPost(USERNAME_BULK_FIND_URI);
+
+        request.setHeaders(DEFAULT_HEADERS);
+        try {
+            request.setEntity(new StringEntity(mapper.writeValueAsString(chunk), ContentType.APPLICATION_JSON));
+        } catch (JsonProcessingException e) {
+            logger.log(
+                    Level.WARNING,
+                    "An error occurred while serializing chunk data...",
+                    e
+            );
+        }
+
+        while (true) {
+            try {
+                Thread.sleep(1000); // we have 600 requests each 10 minutes
+
+                HttpResponse response = client.execute(
+                        request,
+                        data -> {
+                            HttpEntity entity = data.getEntity();
+                            return new HttpResponse(
+                                    data.getCode(),
+                                    entity == null ? "{}" : EntityUtils.toString(entity)
+                            );
+                        }
+                );
+
+                if (response == null || response.result() == null) {
+                    logger.warning("Got an unexpected response, trying again...");
+                    continue;
+                }
+
+                if (response.status() == HttpStatus.SC_FORBIDDEN) {
+                    logger.warning("Seems like the proxy of this HTTP client got forbidden...");
+                    continue;
+                }
+
+                if (response.status() == HttpStatus.SC_TOO_MANY_REQUESTS) {
+                    logger.warning("Got rate-limited, waiting " + configuration.rateLimitedDelay() + "ms!");
+                    Thread.sleep(configuration.rateLimitedDelay());
+                    continue;
+                }
+
+                Instant now = Instant.now();
+
+                List<UsernamesBulkResponse> responses = mapper.readValue(response.result(), new TypeReference<>() {
+                });
+
+                Collection<String> taken = responses.stream()
+                        .map(usernamesBulkResponse -> usernamesBulkResponse.name().toLowerCase())
+                        .collect(Collectors.toSet());
+
+                for (String name : taken) {
+                    lastChecks.register(new LastCheck(
+                            name,
+                            now.toEpochMilli()
+                    ));
+                }
+
+                chunk.stream()
+                        .filter(s -> !taken.contains(s))
+                        .forEach(name -> {
+                            LastCheck lastCheck = lastChecks.remove(name);
+                            dropTimes.register(new NameDropTime(
+                                    name,
+                                    lastCheck == null
+                                            ?
+                                            startedAt.plus(37, ChronoUnit.DAYS).toEpochMilli()
+                                            :
+                                            Instant.ofEpochMilli(lastCheck.when()).plus(37, ChronoUnit.DAYS).toEpochMilli(),
+                                    now.plus(37, ChronoUnit.DAYS).toEpochMilli()
+                            ));
+
+                            logger.info(ConsoleColors.resetting(
+                                    ConsoleColors.GREEN,
+                                    "New name detected as available: " + name + " at: " + now)
+                            );
+                        });
+
+                latch.countDown();
+                logger.info("Got a 200 response from name check request. Total count: " + SUCCESSFUL_REQUESTS.incrementAndGet());
+
+                break;
+            } catch (IOException e) {
+                logger.log(
+                        Level.WARNING,
+                        "An error occurred while executing name check request, trying again...",
+                        e
+                );
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+        }
+    }
+}
--- checker/src/main/java/me/siansxint/sniper/checker/service/ChunkedNameCheckerService.java
+++ checker/src/main/java/me/siansxint/sniper/checker/service/ChunkedNameCheckerService.java
@@ -1,216 +1,96 @@
 package me.siansxint.sniper.checker.service;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import me.siansxint.sniper.checker.Requests;
-import me.siansxint.sniper.checker.TCachedStorage;
+import me.siansxint.sniper.checker.ChunkProcessorTask;
+import me.siansxint.sniper.checker.LastCheckCachedStorage;
 import me.siansxint.sniper.checker.config.Configuration;
-import me.siansxint.sniper.checker.model.LastCheck;
 import me.siansxint.sniper.checker.model.NameDropTime;
-import me.siansxint.sniper.checker.model.UsernamesBulkResponse;
 import me.siansxint.sniper.common.ConsoleColors;
 import me.siansxint.sniper.common.Service;
 import me.siansxint.sniper.common.http.HttpClientSelector;
 import me.siansxint.sniper.common.registry.TRegistry;
-import org.apache.hc.core5.http.ContentType;
-import org.apache.hc.core5.http.io.entity.StringEntity;
 import team.unnamed.inject.Inject;
 import team.unnamed.inject.Named;
 
-import java.net.URI;
 import java.time.Instant;
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.Collections;
 import java.util.List;
-import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.TimeUnit;
-import java.util.concurrent.atomic.AtomicInteger;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import java.util.stream.Collectors;
 
 public class ChunkedNameCheckerService implements Service {
 
-    private static final URI USERNAME_BULK_FIND_URI = URI.create("https://api.minecraftservices.com/minecraft/profile/lookup/bulk/byname");
     private static final int CHUNK_SIZE = 10;
-    private static final AtomicInteger SUCCESSFUL_REQUESTS = new AtomicInteger(0);
 
     private @Inject HttpClientSelector httpClientSelector;
 
-    private @Inject TRegistry<NameDropTime> dropTimes;
-    private @Inject TRegistry<LastCheck> lastChecks;
-    private @Inject TCachedStorage<LastCheck> lastChecksStorage;
+    private @Inject TRegistry<NameDropTime> dropTimesRegistry;
+    private @Inject LastCheckCachedStorage lastChecks;
     private @Inject List<String> names;
 
-    private @Inject
-    @Named("checker") ExecutorService checkerService;
+    @Inject
+    @Named("checker")
+    private ExecutorService checkerService;
 
-    private @Inject Configuration configuration;
     private @Inject ObjectMapper mapper;
+    private @Inject Configuration configuration;
 
     private @Inject Logger logger;
 
-    private Thread thread;
-
     @Override
     public void start() {
-        this.thread = Thread.ofVirtual()
-                .name("Chunked Name Checker Service")
-                .start(
-                        () -> {
-                            while (!checkerService.isTerminated()) {
-                                Collection<Collection<String>> chunks = chunkNames();
-                                logger.info("Chunks to process: " + chunks.size());
+        Thread.ofVirtual()
+                .name("ChunkedNameCheckerService")
+                .start(() -> {
+                    while (!names.isEmpty()) {
+                        Collection<Collection<String>> chunks = chunkNames();
+                        logger.info("Chunks to process: " + chunks.size());
 
-                                CountDownLatch latch = new CountDownLatch(chunks.size());
+                        CountDownLatch latch = new CountDownLatch(chunks.size());
 
-                                for (Collection<String> chunk : chunks) {
-                                    CompletableFuture.supplyAsync(
-                                                    () -> processChunk(
-                                                            httpClientSelector,
-                                                            chunk,
-                                                            configuration,
-                                                            mapper
-                                                    ), checkerService
-                                            )
-                                            .whenComplete((available, throwable) -> {
-                                                latch.countDown();
+                        Instant startedAt = Instant.now();
+                        long start = System.nanoTime();
 
-                                                if (throwable != null || available.isEmpty()) {
-                                                    return;
-                                                }
-
-                                                Instant now = Instant.now();
-
-                                                for (String name : available) {
-                                                    LastCheck check = lastChecksStorage.remove(name);
-                                                    dropTimes.register(new NameDropTime(
-                                                            name,
-                                                            Instant.ofEpochMilli(
-                                                                    check == null
-                                                                            ?
-                                                                            now.toEpochMilli()
-                                                                            :
-                                                                            check.when().toEpochMilli()
-                                                            ),
-                                                            now
-                                                    ));
-
-                                                    logger.info(ConsoleColors.resetting(
-                                                            ConsoleColors.GREEN,
-                                                            "New name detected as available: " + name + ", at: " + now.toString())
-                                                    );
-                                                }
-                                            });
-                                }
-
-                                try {
-                                    latch.await();
-                                } catch (InterruptedException e) {
-                                    logger.log(
-                                            Level.WARNING,
-                                            "An error occurred while waiting for chunks processing...",
-                                            e
-                                    );
-                                }
-
-                                logger.info(ConsoleColors.resetting(ConsoleColors.PURPLE, "Finished processing " + chunks.size() + " chunks!"));
-                                SUCCESSFUL_REQUESTS.set(0);
-                            }
+                        for (Collection<String> chunk : chunks) {
+                            checkerService.submit(new ChunkProcessorTask(
+                                    chunk,
+                                    latch,
+                                    httpClientSelector.next(),
+                                    mapper,
+                                    logger,
+                                    dropTimesRegistry,
+                                    lastChecks,
+                                    configuration,
+                                    startedAt
+                            ));
                         }
-                );
-    }
 
-    @Override
-    public void stop() {
-        if (thread != null) {
-            thread.interrupt();
-        }
-    }
+                        try {
+                            latch.await();
+                        } catch (InterruptedException e) {
+                            logger.log(
+                                    Level.WARNING,
+                                    "An error occurred while waiting for chunks processing...",
+                                    e
+                            );
+                        }
 
-    private Collection<String> processChunk(HttpClientSelector selector,
-                                            Collection<String> chunk,
-                                            Configuration configuration,
-                                            ObjectMapper mapper) {
-        while (true) {
-            Requests.HttpResponse response;
-            try {
-                response = Requests.post(
-                        new StringEntity(mapper.writeValueAsString(chunk), ContentType.APPLICATION_JSON),
-                        selector,
-                        USERNAME_BULK_FIND_URI,
-                        logger
-                );
-            } catch (JsonProcessingException e) {
-                logger.log(
-                        Level.WARNING,
-                        "An error occurred while serializing chunk data...",
-                        e
-                );
-                continue;
-            }
-
-            if (response == null) {
-                logger.warning("Got no HTTP response...");
-                continue;
-            }
-
-            if (response.status() == 200) {
-                Instant now = Instant.now();
-
-                List<UsernamesBulkResponse> responses;
-                try {
-                    responses = mapper.readValue(response.body(), new TypeReference<>() {
-                    });
-                } catch (JsonProcessingException e) {
-                    logger.log(
-                            Level.WARNING,
-                            "An error occurred while reading request body...",
-                            e
-                    );
-                    continue;
-                }
-
-                Collection<String> taken = responses.stream()
-                        .map(usernamesBulkResponse -> usernamesBulkResponse.name().toLowerCase())
-                        .collect(Collectors.toSet());
-
-                for (String name : taken) {
-                    lastChecks.register(new LastCheck(name, now));
-                }
-
-                logger.info("Got a 200 response from name check request. Total count: " + SUCCESSFUL_REQUESTS.incrementAndGet());
-
-                return chunk.stream()
-                        .filter(s -> !taken.contains(s))
-                        .collect(Collectors.toSet());
-            } else if (response.status() == 429) {
-                logger.info("Got rate-limited. Waiting " + configuration.rateLimitDelay() + "ms.");
-                try {
-                    TimeUnit.MILLISECONDS.sleep(configuration.rateLimitDelay());
-                } catch (InterruptedException e) {
-                    logger.log(
-                            Level.WARNING,
-                            "An error occurred while preventing being rate-limited...",
-                            e
-                    );
-                }
-            }
-        }
+                        logger.info(ConsoleColors.resetting(ConsoleColors.PURPLE, "Finished processing " + chunks.size() + " chunks! Took " + TimeUnit.NANOSECONDS.toSeconds(System.nanoTime() - start) + " seconds."));
+                        ChunkProcessorTask.SUCCESSFUL_REQUESTS.set(0);
+                    }
+                });
     }
 
     private Collection<Collection<String>> chunkNames() {
         List<String> randomized = new ArrayList<>(names);
-        Collections.shuffle(randomized); // cant explain why, but this should get better results
 
         Collection<Collection<String>> chunks = new ArrayList<>(CHUNK_SIZE);
         for (int i = 0; i < randomized.size(); i += CHUNK_SIZE) {
-            Collection<String> chunk = new ArrayList<>(randomized.subList(i, Math.min(randomized.size(), i + CHUNK_SIZE)));
-            chunks.add(chunk);
+            chunks.add(new ArrayList<>(randomized.subList(i, Math.min(randomized.size(), i + CHUNK_SIZE))));
         }
 
         return chunks;
