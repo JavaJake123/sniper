9dc0a85a012dc6767f9fe5e6a54f53a5e82488f1
siansxint <joshua1504.jr@gmail.com>
2024-08-31 17:23:43 -0400

    chore(logger): remove thread-name from logger-formatter (should add it back when i get a way to actually show the original thread and not the one from the logger handler)

--- common/src/main/java/me/siansxint/sniper/common/logger/LoggerFormatter.java
+++ common/src/main/java/me/siansxint/sniper/common/logger/LoggerFormatter.java
@@ -1,26 +1,36 @@
 package me.siansxint.sniper.common.logger;
 
+import java.io.PrintWriter;
+import java.io.StringWriter;
 import java.util.Date;
 import java.util.logging.Formatter;
 import java.util.logging.LogRecord;
 
 public class LoggerFormatter extends Formatter {
 
-    private static final String FORMAT = "[%1$tF %1$tT.%1$tL] [%2$s] [%4$s] %3$s %n";
+    private static final String FORMAT = "[%1$tF %1$tT.%1$tL] [%2$s] %3$s %n";
 
     @Override
     public String format(LogRecord record) {
-        StringBuilder sb = new StringBuilder();
-        sb.append(String.format(FORMAT,
+        StringBuilder builder = new StringBuilder();
+        builder.append(String.format(FORMAT,
                 new Date(record.getMillis()),
                 record.getLevel().getLocalizedName(),
-                record.getMessage(),
-                Thread.currentThread().getName()));
+                record.getMessage()));
 
         if (record.getThrown() != null) {
-            sb.append("Exception: ").append(record.getThrown().getMessage()).append("\n");
+            String throwable;
+
+            StringWriter sw = new StringWriter();
+            PrintWriter pw = new PrintWriter(sw);
+            pw.println();
+            record.getThrown().printStackTrace(pw);
+            pw.close();
+            throwable = sw.toString();
+
+            builder.append("Exception: ").append(throwable).append("\n");
         }
 
-        return sb.toString();
+        return builder.toString();
     }
 }
