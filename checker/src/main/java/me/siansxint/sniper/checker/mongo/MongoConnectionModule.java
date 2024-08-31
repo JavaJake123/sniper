3be8dab12a7cd14b316f4035ac6e623daec175af
siansxint <joshua1504.jr@gmail.com>
2024-08-31 17:20:41 -0400

    feat(mongo): set mongo-driver-sync logger level to severe

--- checker/src/main/java/me/siansxint/sniper/checker/mongo/MongoConnectionModule.java
+++ checker/src/main/java/me/siansxint/sniper/checker/mongo/MongoConnectionModule.java
@@ -13,15 +13,13 @@ import team.unnamed.inject.Module;
 import team.unnamed.inject.Provides;
 import team.unnamed.inject.Singleton;
 
-import java.util.logging.Level;
-import java.util.logging.Logger;
-
 public class MongoConnectionModule extends AbstractModule implements Module {
 
     @Provides
     @Singleton
     public MongoClient mongoClient(Configuration configuration, ObjectMapper mapper) {
-        MongoClient client = MongoClients.create(
+        System.setProperty("org.slf4j.simpleLogger.log.org.mongodb.driver", "error");
+        return MongoClients.create(
                 MongoClientSettings.builder()
                         .applyConnectionString(new ConnectionString(configuration.mongoUri()))
                         .codecRegistry(CodecRegistries.fromRegistries(
@@ -30,10 +28,5 @@ public class MongoConnectionModule extends AbstractModule implements Module {
                         ))
                         .build()
         );
-
-        Logger.getLogger("org.mongodb.driver.client")
-                .setLevel(Level.SEVERE);
-
-        return client;
     }
 }
